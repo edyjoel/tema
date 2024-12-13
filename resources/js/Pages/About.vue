e805ba2ac95dc25bf422e73c3fe29dfc56f71605
Edy Joel Xicón <edyxicon@gmail.com>
2024-12-13 00:10:52 -0600

    First version works without ziggy

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+        <Link href="/">Home</Link>
+    </div>
+</template>
+<script>
+import {Link} from '@inertiajs/vue3'
+
+export default {
+    components: {
+        Link,
+    },
+}
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+        <Link href="/about">About</Link>
+    </div>
+</template>
+<script>
+import { Link } from '@inertiajs/vue3'
+
+export default {
+    components: {
+        Link,
+    },
+}
+</script>
