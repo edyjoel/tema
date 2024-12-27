b586423f080d00d903846dd419048e2cfc4f59a7
Edy Joel Xicón <edyxicon@gmail.com>
2024-12-27 12:29:06 -0600

    Add authentication middleware and update routes for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,19 @@ Route::get('/about', function () {
     return Inertia::render('About');
 })->name('about');
 
+Route::get('/', function () {
+    return redirect()->route('admin.dashboard');
+});
+
+Route::get('/forgot-password', function () {
+    return redirect()->route('admin.dashboard');
+});
+
+//Route::middleware('web', 'auth:sanctum', config('jetstream.auth_session'), 'verified')
+
+//Route::middleware('web', 'auth:sanctum', config('jetstream.auth_session'), 'verified')
+
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
-    ->middleware('can:admin.dashboard')
+    ->middleware(['can:admin.dashboard', 'web', 'auth', config('jetstream.auth_session'), 'verified'])
     ->name('admin.dashboard');
