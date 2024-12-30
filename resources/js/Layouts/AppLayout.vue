db8ff5caa4285a491ff5cb68ed0af68d8f23d32e
Edy Joel Xicón <edyxicon@gmail.com>
2024-12-30 14:39:26 -0600

    Enable profile photos feature, integrate Vuetify styles, and update AppLayout for admin dashboard

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,26 +1,40 @@
 <script setup>
-import { ref } from 'vue';
-import { Head, Link, router } from '@inertiajs/vue3';
-import ApplicationMark from '@/Components/ApplicationMark.vue';
+import {ref, watch} from 'vue';
+import {Head, Link, router} from '@inertiajs/vue3';
 import Banner from '@/Components/Banner.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import {useDisplay} from 'vuetify';
 
 defineProps({
     title: String,
 });
 
-const showingNavigationDropdown = ref(false);
+const drawer = ref(false);
+const menu = ref(false);
 
-const switchToTeam = (team) => {
-    router.put(route('current-team.update'), {
-        team_id: team.id,
-    }, {
-        preserveState: false,
-    });
-};
+const headerItems = ref([
+    {
+        text: 'About',
+        value: 'about',
+        route: 'about',
+        icon: 'mdi-home'
+    }
+]);
+
+const userItems = ref([
+    {
+        text: 'Profile',
+        value: 'profile',
+        route: 'profile.show',
+        icon: 'mdi-account'
+    }
+]);
+
+const {mobile} = useDisplay();
+drawer.value = !mobile.value;
+
+watch(mobile, (isMobile) => {
+    drawer.value = !isMobile;
+});
 
 const logout = () => {
     router.post(route('logout'));
@@ -28,25 +42,69 @@ const logout = () => {
 </script>
 
 <template>
-    <div>
-        <Head :title="title" />
-        {{title}}
-
-        <Banner />
-
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-
-            <!-- Page Heading -->
-            <header v-if="$slots.header" class="bg-white dark:bg-gray-800 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
-        </div>
-    </div>
+    <v-layout>
+        <v-app-bar
+            color="primary"
+            prominent
+        >
+            <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Dashboard</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-menu
+                v-model="menu"
+                :close-on-content-click="false"
+                location="bottom right"
+            >
+                <template v-slot:activator="{ props }">
+                    <v-btn icon v-bind="props">
+                        <v-avatar>
+                            <img
+                                :src="$page.props.auth.user.profile_photo_url ? $page.props.auth.user.profile_photo_url : 'https://ui-avatars.com/api/?name=' + $page.props.auth.user.name + '&color=7F9CF5&background=EBF4FF'"
+                                :alt="$page.props.auth.user.name">
+                        </v-avatar>
+                    </v-btn>
+                </template>
+                <v-card min-width="230">
+                    <v-list>
+                        <v-list-item
+                            :prepend-avatar="$page.props.auth.user.profile_photo_url"
+                            subtitle="Admin"
+                            :title="$page.props.auth.user.name"
+                        ></v-list-item>
+                    </v-list>
+                    <v-spacer class="border-b-thin"></v-spacer>
+                    <v-list nav>
+                        <Link v-for="item in userItems" :href="route(item.route)">
+                            <v-list-item color="primary" :prepend-icon="item.icon" :title="item.text"
+                                         :value="item.value"
+                                         :active="route().current(item.route)"></v-list-item>
+                        </Link>
+                        <Link :href="route('logout')" method="post">
+                            <v-list-item color="primary" title="Logout" value="logout"
+                                         prepend-icon="mdi-logout"></v-list-item>
+                        </Link>
+                    </v-list>
+                </v-card>
+            </v-menu>
+        </v-app-bar>
+        <v-navigation-drawer
+            v-model="drawer"
+            :location="$vuetify.display.mobile ? 'left' : undefined"
+        >
+            <v-list nav>
+                <Link v-for="item in headerItems" :href="route(item.route)">
+                    <v-list-item color="primary" :prepend-icon="item.icon" :title="item.text"
+                                 :value="item.value"
+                                 :active="route().current(item.route)"></v-list-item>
+                </Link>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main>
+            <v-container>
+                <slot/>
+            </v-container>
+        </v-main>
+    </v-layout>
+    <Head :title="title"/>
+    <Banner/>
 </template>
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -1,12 +1,8 @@
 <script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
 </script>
 <template>
     <AppLayout title="Panel de administración">
-        <template #header>
-            <h1 class="font-semi-bold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Panel de administración
-            </h1>
-        </template>
         <h1>test</h1>
     </AppLayout>
 </template>
